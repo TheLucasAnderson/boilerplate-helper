0663aa2bf20dd40632e310b1852cd7301a82cfda
TheLucasAnderson <lucasand96@gmail.com>
2024-06-22 23:40:48 -0300

    feat(react_router_dom): add react router dom setup o main file project

---
+++ src/components/About/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles';
+
+export const About = () => {
+  return (
+    <S.Anchor href="https://github.com/TheLucasAnderson" target="_blank">
+      gh/TheLucasAnderson
+    </S.Anchor>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import ResetStyles from './styles/reset.ts';
 import theme from './styles/theme';
-import { App } from './components';
+import { App, About } from './components';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <ResetStyles />
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 );
