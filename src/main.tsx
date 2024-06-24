3c93b3782800611f9880c86035d7c920f10b3521
TheLucasAnderson <lucasand96@gmail.com>
2024-06-24 20:34:41 -0300

    feat(many): lazy load - Pages settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { Navigation } from './routes.tsx';
 import ResetStyles from './styles/reset.ts';
 import theme from './styles/theme';
-import { App, About } from './components';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <ResetStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      <Navigation />
     </ThemeProvider>
   </React.StrictMode>,
 );
---
+++ src/pages/home.tsx
@@ -0,0 +1,8 @@
+import { App } from '../components';
+
+export default () => (
+  <div>
+    <p>Home Page</p>
+    <App />
+  </div>
+);
