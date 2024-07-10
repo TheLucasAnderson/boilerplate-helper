b89c55e53b0559a8e024cf2848f736bb92cb0791
TheLucasAnderson <lucasand96@gmail.com>
2024-07-09 23:57:38 -0300

    fix(tests): Apply i18n config on tests utils

--- src/utils/tests/index.tsx
+++ src/utils/tests/index.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from '../../styles/theme';
 import { render } from '@testing-library/react';
+import '../../../i18n.config.ts';
 
 export const renderWithThemeProvider = (component: ReactElement) => {
   return render(<ThemeProvider theme={theme}>{component}</ThemeProvider>);
